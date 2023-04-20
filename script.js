787d6229b0163b06a5f63a09061b6dbf0529a8eb
JMeltzer92 <jmeltze1@umd.edu>
2023-04-20 10:50:16 -0400

    text styling

--- script.js
+++ script.js
@@ -13,67 +13,74 @@ const data = [
         graf: `Starting July 1st there are going to be a lot more people in the market for cannabis. An influx of new customers and the revenue that they’ll bring with them is an attractive incentive for businesses, particularly early market entrants who will have the first shot at building relationships with retail customers or other cannabis businesses.`,
         newButtons: [4, 5, 6],
         nextQuestion: "I want to be a...",
-        imgSrc: "images\\seller.png"
+        imgSrc: "images\\seller.png",
+        alt: "An impressionist-style rendering of a woman behind a counter discussing a transaction with a customer next to a large cannabis plant."
     },
     {id: 2,
         title: "Medical",
         graf: `Good news! Not a lot is going to change for you. You will continue to be able to buy from your licensed medical dispensary and not a lot should change about how the process looks. One key difference: taxation. Cannabis will be taxed at 9%.`,
         newButtons: [],
         nextQuestion: "",
-        imgSrc: "images\\medical.png"
+        imgSrc: "images\\medical.png",
+        alt: "An impressionist-style rendering of a woman in green scrubs and a stethoscope discussing medicinal cannabis with a patient."
     },
     {id: 3,
         title: "Recreational",
-        graf: `If you’ve never used cannabis, before walking into a dispensary to buy, you’ll have some important things to consider, most importantly your personal health and safety.</p><blockquote></blockquote><p class = 'graf'>Thankfully, one of legalization’s major benefits is the greater sense of certainty it provides to consumers that they’re getting exactly what is on the label.</p>
-        <blockquote>'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, resquietat in pace en nomine file et spiritu sancti.'<br> - Wayne Gretzky</blockquote>
+        graf: `If you’ve never used cannabis, before walking into a dispensary to buy, you’ll have some important things to consider, most importantly your personal health and safety.</p>
+        <p class = 'graf'>Thankfully, one of legalization’s major benefits is the greater sense of certainty it provides to consumers that they’re getting exactly what is on the label.</p>
+        <blockquote><span class = "quote">'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, resquietat in pace en nomine file et spiritu sancti.'</span><br><span class = "source">- Wayne Gretzky</span></blockquote>
         <p class = 'graf'>One of legalization’s major benefits is the greater sense of certainty it provides to consumers that they’re getting exactly what is on the label, a factor that should be helpful in driving consumers away from the illicit market, which can normally offer no such guarantees.`,
         newButtons: [],
         nextQuestion: "",
-        imgSrc: "images\\recreational.png"
+        imgSrc: "images\\recreational.png",
+        alt: "An impressionist-style rendering of a man riding a bicycle along a waterfront with a large container ship in the distance."
     },
     {id: 4,
         title: "Grower",
-        graf: `Growers will have an especially important role to play in Maryland’s recreational cannabis industry. Because cannabis is still federally illegal, it remains pretty much impossible to move cannabis over state borders or for bulk buyers and sellers in different states to do business with each other. As such, growers will be enormously important in making sure there is adequate cannabis supply to meet new demand and that consumers have a safe product to enjoy.<br>
-        Licensed growers will be able to operate between 10,000 and 300,000 square feet of indoor grow space, enough to produce an estimated over half a million pounds of cannabis per year.<br>
-        Like dispensaries, prospective growers who want to deal in smaller amounts have the option to apply for a “micro-license” which will grant them the opportunity to grow any amount under 10,000 square feet.`, 
+        graf: `Growers will have an especially important role to play in Maryland’s recreational cannabis industry. Because cannabis is still federally illegal, it remains pretty much impossible to move cannabis over state borders or for bulk buyers and sellers in different states to do business with each other. As such, growers will be enormously important in making sure there is adequate cannabis supply to meet new demand and that consumers have a safe product to enjoy.</p>
+        <p class = 'graf'>Licensed growers will be able to operate between 10,000 and 300,000 square feet of indoor grow space, enough to produce an estimated over half a million pounds of cannabis per year.</p>
+        <p class = 'graf'>Like dispensaries, prospective growers who want to deal in smaller amounts have the option to apply for a “micro-license” which will grant them the opportunity to grow any amount under 10,000 square feet.`, 
         newButtons: [7], 
         nextQuestion: "What now?",
-        imgSrc: "images\\grow_house.png"
+        imgSrc: "images\\grow_house.png",
+        alt: "An impressionist-style rendering of a greenhouse containing several cannabis plants."
     },
     {id: 5,
         title: "Processor",
-        graf: `Cannabis processors will serve as the bridge between growers and retailers. Processors will perform all the steps necessary to get cannabis on shelves including drying, curing, baking, and packaging. Processors are especially important for products like edibles and tinctures where the actual cannabis plant is highly processed.<br>
-        Licensed processors will be allowed to process anywhere above 1,000 pounds of cannabis per year under the new law. Like with other types of licenses there will be \“micro-licenses\” that allow licensees to process up to 1,000 pounds yearly.`,
+        graf: `Cannabis processors will serve as the bridge between growers and retailers. Processors will perform all the steps necessary to get cannabis on shelves including drying, curing, baking, and packaging. Processors are especially important for products like edibles and tinctures where the actual cannabis plant is highly processed.</p>
+        <p class = 'graf'>Licensed processors will be allowed to process anywhere above 1,000 pounds of cannabis per year under the new law. Like with other types of licenses there will be \“micro-licenses\” that allow licensees to process up to 1,000 pounds yearly.</p>`,
         newButtons: [7],
         nextQuestion: "What now?",
-        imgSrc: "images\\processors.png"
+        imgSrc: "images\\processors.png",
+        alt: "An impressionist-style rendering of a group of workers in white coats, goggles and respirators processing cannabis."
     },
     {id: 6,
         title: "Distributor/Dispensary",
-        graf: `Dispensaries will be the most people-facing arm of the cannabis industry. Dispensaries and dispensary owners will play a large role in making sure there is adequate supply for the avalanche of demand that lawmakers expect starting July 1st and providing the kind of valuable advice that will help consumers ensure they are practicing healthy consumption habits.<br>Dispensary licenses will make business owners eligible to operate a physical retail location and sell cannabis products so the next step after getting licensed will be to find a supplier and a storefront.<br>
-        The bill also provides an option for prospective sellers who don’t necessarily want to operate a store. “Micro-licenses” will allow businesses to operate a delivery service for cannabis products as long as that business has fewer than 10 employees.`,
+        graf: `Dispensaries will be the most people-facing arm of the cannabis industry. Dispensaries and dispensary owners will play a large role in making sure there is adequate supply for the avalanche of demand that lawmakers expect starting July 1st and providing the kind of valuable advice that will help consumers ensure they are practicing healthy consumption habits.</p>
+        <p class = 'graf'>Dispensary licenses will make business owners eligible to operate a physical retail location and sell cannabis products so the next step after getting licensed will be to find a supplier and a storefront.</p>
+        <p class = 'graf'>The bill also provides an option for prospective sellers who don’t necessarily want to operate a store. “Micro-licenses” will allow businesses to operate a delivery service for cannabis products as long as that business has fewer than 10 employees.`,
         newButtons: [7],
         nextQuestion: "What now?"
     },
     {id: 7,
         title: "Get Licensed",
-        graf: `You’ve done all your homework. You’ve got your finances in order. You’ve studied the viability of the idea. You put a business plan together. You have investors lined up. The final step before you can become a bonafide cannabis entrepreneur is a seal of approval from the state in the form of a license to operate.<br>
-        Competition for licenses is expected to be fierce given high demand for cannabis. That said, the state still needs operators who will be ready to meet the state’s demand on day one. To that aim, current licensed medical operators will have the opportunity to convert their license to a recreational one by paying a conversion fee in line with their revenue. Though some medical operators feel this amount is too high.</p>
-        <blockquote>'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, resquietat in pace en nomine file et spiritu sancti.'<br> - Wayne Gretzky</blockquote>
+        graf: `You’ve done all your homework. You’ve got your finances in order. You’ve studied the viability of the idea. You put a business plan together. You have investors lined up. The final step before you can become a bonafide cannabis entrepreneur is a seal of approval from the state in the form of a license to operate.</p>
+        <p class = 'graf'>Competition for licenses is expected to be fierce given high demand for cannabis. That said, the state still needs operators who will be ready to meet the state’s demand on day one. To that aim, current licensed medical operators will have the opportunity to convert their license to a recreational one by paying a conversion fee in line with their revenue. Though some medical operators feel this amount is too high.</p>
         <p class = 'graf'>Lawmakers have also written provisions into the bill to prioritize applicants potentially affected by the war on drugs, a series of aggressive state and federal drug enforcement policies that disproportionately affected Black Americans and served as one of the primary drivers of mass incarceration.</p>
-        <blockquote>'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, resquietat in pace en nomine file et spiritu sancti.'<br> - Wayne Gretzky</blockquote>
-        <p class = 'graf'>The first round of new licenses will be reserved for these so-called \“social equity applicants\” and will be issued before January 1st 2024. Though rules around race-based set-asides make it complicated to specifically prioritize Black and brown applicants, lawmakers crafted the definition of “social equity applicants” to make sure those most affected have the first spots in the line for a license.<br>
-        To be defined as “social equity applicant” (and if your application is successful a “social equity licensee), applicants will have maintain at least a 65% ownership stake in their business and fulfill at least one of these requirements:</p>
+        <blockquote><span class = "quote">'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, resquietat in pace en nomine file et spiritu sancti.'</span><br><span class = "source">- Wayne Gretzky</span></blockquote>
+        <p class = 'graf'>The first round of new licenses will be reserved for these so-called \“social equity applicants\” and will be issued before January 1st 2024. Though rules around race-based set-asides make it complicated to specifically prioritize Black and brown applicants, lawmakers crafted the definition of “social equity applicants” to make sure those most affected have the first spots in the line for a license.</p>
+        <p class = 'graf'>To be defined as “social equity applicant” (and if your application is successful a “social equity licensee), applicants will have maintain at least a 65% ownership stake in their business and fulfill at least one of these requirements:</p>
         <ul class = 'lineup-list'>
             <li>Have lived in a “disproportionately impacted area” for at least 5 of the last 10 years</li>
             <li>Attended public school in a “disproportionately impacted area” for at least 5 years</li>
             <li>Attended a 4-year higher education institution in Maryland where at least 40% of students are eligible for Pell Grants</li>
         </ul>
-        <p class = 'graf'>The bill defines a “disproportionately impacted area” as a means a geographic area outlined by the state cannabis commission’s new office of social equity that had above 150% of the state’s 10-year average for cannabis possession charges. Licenses for second-round applicants will begin to be issued by May of 2024.<br>
-        Applying for a license will also come at a cost. A standard license application will cost prospective licensees $5,000 while an application for a “micro-license” will run $1,000. `,
+        <p class = 'graf'>The bill defines a “disproportionately impacted area” as a means a geographic area outlined by the state cannabis commission’s new office of social equity that had above 150% of the state’s 10-year average for cannabis possession charges. Licenses for second-round applicants will begin to be issued by May of 2024.</p>
+        <p class = 'graf'>Applying for a license will also come at a cost. A standard license application will cost prospective licensees $5,000 while an application for a “micro-license” will run $1,000. `,
         newButtons: [],
         nextQuestion: "",
-        imgSrc: "images\\licensing.png"
+        imgSrc: "images\\licensing.png",
+        alt: "An impressionist-style rendering of a woman handing something to a patent clerk in a tie while a law enforcement officer looks on in the background."
     }
 ]
 
@@ -110,13 +117,11 @@ const pickChoice = function (idChosen) {
     const newStory = document.createElement("div");
     newStory.classList.add("story-slide");
     if (workingData.imgSrc) {newStory.innerHTML = `
-    <p class = 'text-center hed'>${workingData.title}</p>
-    <div class = 'clearfix'>
-        <img class = 'story-pic' src = '${workingData.imgSrc}'></img>
-        <p class = 'graf with-pic'>${workingData.graf}</p>
-    </div>
+    <p class = 'text-center sub-hed'>${workingData.title}</p>
+    <img class = 'story-pic' src = '${workingData.imgSrc}' alt = '${workingData.alt}'></img>
+    <p class = 'graf with-pic'>${workingData.graf}</p>
     `} else {newStory.innerHTML = `
-    <p class = 'text-center hed'>${workingData.title}</p>
+    <p class = 'text-center sub-hed'>${workingData.title}</p>
     <p class = 'graf'>${workingData.graf}</p>
     `};
     body.append(newStory);
